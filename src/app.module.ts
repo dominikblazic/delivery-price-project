bd9664f6ceb714428ddfeaea48f9417b2255d653
Dominik Blažić <dominik.blazic@infinum.com>
2023-11-14 15:20:48 +0100

    Added .env to gitignore.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 
-const environment = process.env.NODE_ENV || 'default';
-
 @Module({
   imports: [
     ConfigModule.forRoot({
@@ -13,7 +11,7 @@ const environment = process.env.NODE_ENV || 'default';
       isGlobal: true,
       load: [
         () => {
-          return require(`../config/${environment}`).default;
+          return require(`../config/configuration`).default;
         },
       ],
     }),
